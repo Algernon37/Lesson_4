c0be26be6dcf0a323707c2ad900ee3c0ee909022
Ivan Esin <ivan.esin.78@mail.ru>
2023-02-07 11:02:35 +0400

    Второе задание

---
+++ Project2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.WriteLine("Введите вообще любое число");
+int A = Convert.ToInt32(Console.ReadLine());
+int Main(int arg1)
+{
+int sum  = 0;
+for (int i = 0; i <= arg1; i++)
+{
+    sum = sum + (arg1 % 10);
+    arg1 = arg1 / 10;
+}
+return sum;
+}
+Console.WriteLine($"Сумма всех цифр в числе {A} = {Main(A)}");
